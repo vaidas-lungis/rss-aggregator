9bd27907afdb64f1a9c0fa62a718ae4badb4add5
vaidas-lungis <vaidas.lungis+github@gmail.com>
2018-04-30 08:26:51 +0300

    Scaffold Auth but remove registration routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/home', 'HomeController@index')->name('home');
