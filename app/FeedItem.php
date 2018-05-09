7f1b6ba1ed62ab116afd050c39f0d3a2dab9da5b
vaidas-lungis <vaidas.lungis+github@gmail.com>
2018-05-09 08:27:08 +0300

    Add One To Many (Inverse) relationships to feed and feed items

--- app/FeedItem.php
+++ app/FeedItem.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class FeedItem extends Model
 {
     protected $guarded = [];
+
+    public function feed()
+    {
+        return $this->belongsTo(Feed::class);
+    }
 }
