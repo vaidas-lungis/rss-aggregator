08c9f55ca44c9e30cbf665830e166de5b43b2ba2
vaidas-lungis <vaidas.lungis+github@gmail.com>
2018-05-21 08:38:44 +0300

    Update TrustProxies for Heroku
    https://devcenter.heroku.com/articles/getting-started-with-laravel#trusting-the-load-balancer

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,22 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
 use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array
-     */
-    protected $proxies;
-
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var string
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $proxies = '*';
+    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
 }
